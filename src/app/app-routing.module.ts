41f63f36f482e989383bd3eb5c9360b472c4f8ea
NguyenHoang292211 <nguyenhoang13166@gmail.com>
2021-06-30 00:19:03 +0700

    learning and wallet screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseLearningScreenComponent } from './page/course-learning-screen/course-learning-screen.component';
+import { DetailCourseScreenComponent } from './page/detail-course-screen/detail-course-screen.component';
+import { HomeScreenComponent } from './page/home-screen/home-screen.component';
+import { WalletScreenComponent } from './page/wallet-screen/wallet-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'detail-course', component: DetailCourseScreenComponent
+  },
+  {
+    path:'learning', component: CourseLearningScreenComponent
+  },
+  {
+    path:'home', component:HomeScreenComponent
+  },
+  {
+    path:'', redirectTo:'/home', pathMatch:'full'
+  },
+  {
+    path:'wallet', component:WalletScreenComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page/course-learning-screen/course-learning-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-course-learning-screen',
+  templateUrl: './course-learning-screen.component.html',
+  styleUrls: ['./course-learning-screen.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class CourseLearningScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
